d2d1c8316d1375c00a9d55721edfe2f3b7297ee6
Angelica Hernandez <gel.lorraine@gmail.com>
2021-07-24 00:49:42 -0700

    mongodb

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const db = require('../models');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
+  useFindAndModify: false,
   useUnifiedTopology: true,
-  useFindAndModify: false
 });
 
 const workoutSeed = [
